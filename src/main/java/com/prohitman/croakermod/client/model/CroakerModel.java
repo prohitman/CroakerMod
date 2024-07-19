3da0b1cb0b5a30067b436a0a303379f6d4989777
Amine Lahnin <lahnamin@gmail.com>
2024-07-19 15:23:06 +0100

    Added Head/Neck rotations

--- src/main/java/com/prohitman/croakermod/client/model/CroakerModel.java
+++ src/main/java/com/prohitman/croakermod/client/model/CroakerModel.java
@@ -2,9 +2,14 @@ package com.prohitman.croakermod.client.model;
 
 import com.prohitman.croakermod.CroakerMod;
 import com.prohitman.croakermod.server.entity.CroakerEntity;
+import net.minecraft.client.model.PigModel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class CroakerModel extends AnimatedGeoModel<CroakerEntity> {
     @Override
@@ -20,5 +25,22 @@ public class CroakerModel extends AnimatedGeoModel<CroakerEntity> {
     @Override
     public ResourceLocation getAnimationResource(CroakerEntity animatable) {
         return new ResourceLocation(CroakerMod.MODID, "animations/croaker.animation.json");
+    }
+
+    @Override
+    public void setCustomAnimations(CroakerEntity animatable, int instanceId, AnimationEvent animationEvent) {
+        super.setCustomAnimations(animatable, instanceId, animationEvent);
+        IBone head = this.getAnimationProcessor().getBone("Head");
+        IBone neck1 = this.getAnimationProcessor().getBone("Neck1");
+
+        if (head != null) {
+            EntityModelData extraData = (EntityModelData) animationEvent.getExtraDataOfType(EntityModelData.class).get(0);
+            head.setRotationY((extraData.netHeadYaw / 2) * Mth.DEG_TO_RAD);
+            head.setRotationX((extraData.headPitch / 2) * Mth.DEG_TO_RAD);
+            if(neck1 != null){
+                neck1.setRotationY((extraData.netHeadYaw) * Mth.DEG_TO_RAD);
+                neck1.setRotationX((extraData.headPitch) * Mth.DEG_TO_RAD);
+            }
+        }
     }
 }
